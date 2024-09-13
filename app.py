2f1d16cfb96b5c13d54f3d88baf8f520d9599ce0
lcx6592183 <lcx6592183@gmail.com>
2024-09-14 08:18:30 +1200

    change to flask

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+# 指定模板文件夹和静态文件夹的路径
+app = Flask(__name__, template_folder="/templates", static_folder="/static")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
