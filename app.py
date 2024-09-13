6b4a011e6eccea74bf2c9d438453271eaf8d52c7
lcx6592183 <lcx6592183@gmail.com>
2024-09-14 08:39:04 +1200

    fix template derectory bug

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 # 指定模板文件夹和静态文件夹的路径
-app = Flask(__name__, template_folder="/templates", static_folder="/static")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route('/')
 def index():
