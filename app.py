35138a21848fbc634674d7257a61cff155cc29ed
lcx6592183 <lcx6592183@gmail.com>
2024-10-02 16:50:50 +1300

    ready to fillin text

--- app.py
+++ app.py
@@ -7,5 +7,25 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 def index():
     return render_template('index-2.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses-2.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
